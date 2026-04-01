64102d33518965f6020cddd2a3b268927da02764
sanjibkmaity1982 <sanjibkmaity1982@gmail.com>
2026-04-01 11:50:57 +0800

    Remove PWA support from Next.js configuration
    Removed PWA configuration from next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,31 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-  buildExcludes: [
-    /app-build-manifest\.json$/,
-    /middleware-manifest\.json$/,
-    /build-manifest\.json$/,
-    /react-loadable-manifest\.json$/,
-  ],
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "sahaibat-kader-v1",
-        expiration: { maxEntries: 200, maxAgeSeconds: 24 * 60 * 60 },
-        networkTimeoutSeconds: 4,
-      },
-    },
-  ],
-});
-
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
